9824e086e06cc4e951689c8907b2a061d9f3c34f
Lucas Gyan Coloda <37116457+Lucas-Coloda@users.noreply.github.com>
2020-03-17 22:11:14 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/exercicio_android_1/MainActivity.java
+++ app/src/main/java/com/example/exercicio_android_1/MainActivity.java
@@ -10,24 +10,38 @@ import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 
-import org.json.JSONObject;
+import org.json.JSONArray;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    // URLs
     private static final String ENDPOINT = "http://www.transparencia.gov.br/api-de-dados/";
     private static final String BOLSA_FAMILIA_MUNICIPIO = "bolsa-familia-por-municipio";
-    private static final String PARAM_CODIGO_MUNICIPIO = "&codigoIbge=";
-    private static final Integer CODIGO_PR_CURITIBA = 4106902;
-    private static final String PARAM_MES_ANO = "?mesAno=";
-    private static final String PARAM_PAGINA = "&pagina=";
 
+    // URI rquest params
+    private static final String PARAM_CODIGO_MUNICIPIO = "codigoIbge";
+    private static final Integer CODIGO_PR_CURITIBA = 4106902;
+    private static final String PARAM_MES_ANO = "mesAno";
+    private static final String PARAM_PAGINA = "pagina";
+
+    // URI utils
+    private static final String URI_INITIALIATOR = "?";
+    private static final String URI_EQUALS_TO = "=";
+    private static final String URI_AND = "&";
+
+    // View objects
     private TextView textView;
     private List<String> names;
+
+    // Other vars
+    private static final int MONTH_IN_MILISECONDS = 30 * 24 * 60 * 60 * 1000 * -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,36 +51,50 @@ public class MainActivity extends AppCompatActivity {
 
         names = new ArrayList<>();
     }
+
     private void carregarDados() {
-        for (int i = 1; i <= 12; i++) {
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+
+        Date date = new Date();
+
+        Integer currentMonth = Integer.valueOf(monthFormat.format(date.getTime()));
+        Integer currentYear = Integer.valueOf(yearFormat.format(date.getTime()));
+
+        for (int i = 0; i < 12; i++) {
+            String month = String.format("%02d", (((i + currentMonth) % 12) + 1));
+            String year = Long.valueOf(month) > currentMonth ? String.valueOf(currentYear - 1) : String.valueOf(currentYear);
+
             String url = ENDPOINT + BOLSA_FAMILIA_MUNICIPIO;
-            url += PARAM_CODIGO_MUNICIPIO + CODIGO_PR_CURITIBA;
-            url += PARAM_MES_ANO + 201903;
-            url += PARAM_PAGINA + 1;
+            url += URI_INITIALIATOR + PARAM_CODIGO_MUNICIPIO + URI_EQUALS_TO + CODIGO_PR_CURITIBA;
+            url += URI_AND + PARAM_MES_ANO + URI_EQUALS_TO + year + month;
+            url += URI_AND + PARAM_PAGINA + URI_EQUALS_TO + 1;
+            Log.i("INFO", url);
             generateRequest(url);
-            this.showData();
         }
+
+        this.showData();
     }
 
+    // View suport methods
     private void showData() {
-       Log.i("INFO", "oi");
+        Log.i("INFO", "oi");
     }
 
-    // Event captured from view
     public void btnCarregarEvent(View v) {
         carregarDados();
     }
 
     // Services
     public void generateRequest(String url) {
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, successResponse(), errorResponse());
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, successResponse(), errorResponse());
         APISingleton.getInstance(this).addToRequestQueue(request);
     }
 
-    public Response.Listener<JSONObject> successResponse() {
-        return new Response.Listener<JSONObject>() {
+    public Response.Listener<JSONArray> successResponse() {
+        return new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
+            public void onResponse(JSONArray response) {
                 Log.i("INFO", response.toString());
             }
         };
